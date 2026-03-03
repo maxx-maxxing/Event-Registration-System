afd0c7254ab99d6df2096dc1a85c3fa7668cd6a9
maxx-maxxing <maxxlazos93@gmail.com>
2026-03-03 07:44:11 -0700

    Initial Project Setup

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter your full name: ");
+string name = Console.ReadLine() ?? "";
+// ^^ FIXME: Use string.IsNullOrWhiteSpace(string value) for input handling
+
+Console.WriteLine("Age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+// ^^ FIXME: Use try/catch OR TryParse, and range based check
+
+Console.WriteLine("How many days will you be attending the conference? " +
+                  "(Press 1, 2, or 3)");
+int daysAttended = Convert.ToInt32(Console.ReadLine());
+// ^^ FIXME: Use try/catch OR TryParse, and range based check
+
+
+
+
+
+
